f5c0eda7365d0da7209a22bd868298a5bab59979
PhiHung99 <nguyenphihung1310Wgmail.com>
2021-01-27 19:27:15 +0800

    uppdate Cas

---
+++ Module 2/FuramaResort/src/Models/Services.java
@@ -0,0 +1,12 @@
+package Models;
+
+public abstract class Services {
+    // abstract lop truu tuong
+    private String id;
+    private String name;
+    private String area;
+    private String price;
+    private String amount;
+    private String typeRents;
+
+}
